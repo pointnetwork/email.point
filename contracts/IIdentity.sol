7041c8b6a3dc2c1bf18c6dff2e6b4a83a2cf29db
Nicolás Bevilacqua <nicobevilacqua@gmail.com>
2022-06-21 10:11:36 -0300

    interface moved

--- contracts/IIdentity.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-pragma experimental ABIEncoderV2;
-
-interface IIdentity {
-    function isIdentityDeployer(string memory, address) external returns (bool);
-}
--- contracts/PointEmail.sol
+++ contracts/PointEmail.sol
@@ -6,7 +6,10 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "./IIdentity.sol";
+
+interface IIdentity {
+    function isIdentityDeployer(string memory, address) external returns (bool);
+}
 
 /**
  * @dev Implementation of email.point's smart contract.
